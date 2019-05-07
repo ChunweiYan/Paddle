4f688dfa355a4379f07bf6f5ef0c5278bb02ac4e
Shixiaowei02 <39303645+Shixiaowei02@users.noreply.github.com>
2019-05-07 08:05:04 +0000

    fix hvy call

--- paddle/fluid/lite/api/light_api_test.cc
+++ paddle/fluid/lite/api/light_api_test.cc
@@ -27,7 +27,7 @@ TEST(LightAPI, load) {
   predictor.Build(model_dir);
 
   auto* input_tensor = predictor.GetInput(0);
-  input_tensor->Resize({100, 100});
+  input_tensor->Resize(DDim(std::vector<int64_t>{100, 100}));
   auto* data = input_tensor->mutable_data<float>();
   for (int i = 0; i < 100 * 100; i++) {
     data[i] = i;
